7966fe999ba7f6c5c468e04565c0bbaa9124c69a
GreyWolnick <52846222+GreyWolnick@users.noreply.github.com>
2023-02-15 12:52:02 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -138,7 +138,8 @@ def multiclass_outlier_noisify(x, y, transform, nb_classes=10, random_state=1):
         adds gross outliers to training labels
     """
 
-    print("HERE")
+    print(x.shape, y.shape)
+    print(x.shape[0], y.shape[0])
 
     outlier = Outlier(784, 200, nb_classes)  # make these non-static
     unflatten = torch.nn.Unflatten(0, (nb_classes, nb_classes))
