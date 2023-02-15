529a8a0c4bcd149b4172ba08870d337e4bae8569
GreyWolnick <52846222+GreyWolnick@users.noreply.github.com>
2023-02-15 12:27:44 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -139,6 +139,8 @@ def multiclass_outlier_noisify(x, y, transform, nb_classes=10, random_state=1):
 
     new_y = y.copy()
 
+    print("OLD:", y)
+
     for idx in np.arange(x.shape[0]):
         i = y[idx]
 
@@ -176,7 +178,6 @@ def noisify_multiclass_symmetric(y_train, x_train, noise, outlier_noise, transfo
         # how do I want to split these labels so outliers are not used in multiclass_noisify
         y_train_outlier = multiclass_outlier_noisify(x_train[sample_idx, :], y_train[sample_idx, :], transform=transform,
                                                      nb_classes=nb_classes, random_state=random_state)
-        print(y_train_outlier)
         exit()
         y_train_noisy = multiclass_noisify(y_train, P=P, random_state=random_state)
         actual_noise = (y_train_noisy != y_train).mean()
