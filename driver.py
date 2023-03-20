c1cdc328a676d48605cf10e85d8ce1be32221335
GreyWolnick <52846222+GreyWolnick@users.noreply.github.com>
2023-03-19 22:31:26 -0700

    Create driver.py

---
+++ driver.py
@@ -0,0 +1,27 @@
+import subprocess
+
+dataset = 'cifar10'
+indep_noise_rate = 0.2
+dep_noise_rate = 0.4
+
+for q in range(5, 10):
+    q = q / 10.0
+    cmd = f'python3 gce.py --dataset {dataset} --indep_noise_rate {indep_noise_rate} --dep_noise_rate {dep_noise_rate} --q {q} --k {k}'
+    subprocess.run(cmd.split())
+
+q = 0.7
+
+for k in range(1, 5):
+    k = k / 10.0
+    cmd = f'python3 gce.py --dataset {dataset} --indep_noise_rate {indep_noise_rate} --dep_noise_rate {dep_noise_rate} --q {q} --k {k}'
+    subprocess.run(cmd.split())
+
+k = 0.2
+
+for lam in [0.0001, 0.001, 0.00001]:
+    cmd = f'python3 gce.py --dataset {dataset} --indep_noise_rate {indep_noise_rate} --dep_noise_rate {dep_noise_rate} --q {q} --k {k} --lam {lam}'
+    subprocess.run(cmd.split())
+
+
+
+
