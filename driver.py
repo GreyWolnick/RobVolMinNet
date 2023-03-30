64cf9581a09513b16776f6c5a52937a2d893874b
GreyWolnick <52846222+GreyWolnick@users.noreply.github.com>
2023-03-29 18:52:43 -0700

    Update driver.py

--- driver.py
+++ driver.py
@@ -10,6 +10,8 @@ lam = 0.00001
 for dataset in datasets:
     cmd = f'python3 gce.py --dataset {dataset} --indep_noise_rate {indep_noise_rate} --dep_noise_rate {dep_noise_rate} --q {q} --k {k} --lam {lam}'
     subprocess.run(cmd.split())
+    cmd = f'python3 gce.py --loss_func ce --dataset {dataset} --indep_noise_rate {indep_noise_rate} --dep_noise_rate {dep_noise_rate} --q {q} --k {k} --lam {lam}'
+    subprocess.run(cmd.split())
 
 # q = 0.7
 #
