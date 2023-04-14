2f8d979f4cd4f778bbd3b3ccac18fc4655686e42
GreyWolnick <52846222+GreyWolnick@users.noreply.github.com>
2023-04-13 23:28:56 -0700

    Update driver.py

--- driver.py
+++ driver.py
@@ -7,22 +7,15 @@ k = 0.4
 q = 0.5
 lam = 0.00001
 
-for dataset in datasets:
-    cmd = f'python3 gce.py --dataset {dataset} --indep_noise_rate {indep_noise_rate} --dep_noise_rate {dep_noise_rate} --q {q} --k {k} --lam {lam}'
-    subprocess.run(cmd.split())
-    cmd = f'python3 gce.py --loss_func ce --dataset {dataset} --indep_noise_rate {indep_noise_rate} --dep_noise_rate {dep_noise_rate} --q {q} --k {k} --lam {lam}'
-    subprocess.run(cmd.split())
+print("THIS IS A TEST")
 
-# q = 0.7
-#
-# for k in range(1, 5):
-#     k = k / 10.0
-#     cmd = f'python3 gce.py --dataset {dataset} --indep_noise_rate {indep_noise_rate} --dep_noise_rate {dep_noise_rate} --q {q} --k {k}'
-#     subprocess.run(cmd.split())
-#
-# k = 0.2
-#
-# for lam in [0.0001, 0.001, 0.00001]:
+for i in range(10):
+    print(i)
+
+# for dataset in datasets:
 #     cmd = f'python3 gce.py --dataset {dataset} --indep_noise_rate {indep_noise_rate} --dep_noise_rate {dep_noise_rate} --q {q} --k {k} --lam {lam}'
 #     subprocess.run(cmd.split())
+#     cmd = f'python3 gce.py --loss_func ce --dataset {dataset} --indep_noise_rate {indep_noise_rate} --dep_noise_rate {dep_noise_rate} --q {q} --k {k} --lam {lam}'
+#     subprocess.run(cmd.split())
+
 
