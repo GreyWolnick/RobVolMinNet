3973e5a233d7cb82a48230ac82d6ff862e7536bc
GreyWolnick <52846222+GreyWolnick@users.noreply.github.com>
2023-05-04 15:50:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -204,9 +204,9 @@ def checkpoint(acc, epoch, net, type="gce"):
     torch.save(state, './checkpoint/ckpt.t7.' + type)
 
 def maximum_volume_regularization(H):
-    print("SHAPE", H.shape, H.t().shape)
     HH = torch.mm(H.t(), H)
     regularizer_loss = -torch.log(torch.linalg.det(HH))
+    print("BEFORE:", regularizer_loss)
     return regularizer_loss
 
 def minimum_volume_regularization(T):
