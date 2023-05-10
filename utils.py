322b990b33c0073c30f31e55fe8243773ab051a2
GreyWolnick <52846222+GreyWolnick@users.noreply.github.com>
2023-05-09 20:59:49 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -110,7 +110,7 @@ def noisify(train_data, train_labels, seed, noise_rate, feature_size, percent_in
     noisy_labels = []
     flag_instance_indep_noise = []
     for i, sample_numpy in enumerate(train_data):
-        print('##########################################################')
+        # print('##########################################################')
         PILconv = T.ToPILImage()
         if feature_size == 28 * 28:
             sample_numpy = sample_numpy.reshape((28, 28))
@@ -156,6 +156,8 @@ def noisify(train_data, train_labels, seed, noise_rate, feature_size, percent_in
         # print(train_labels[i])
 
         noisy_labels.append(np.random.choice(np.arange(num_class), p=p_all))
+
+    print("done")
 
     over_all_noise_rate = 1 - float(torch.tensor(train_labels).eq(torch.tensor(noisy_labels)).sum()) / train_data.shape[
         0]
