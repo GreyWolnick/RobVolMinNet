9f1c2b22c12df8faa5983582ddb6fbd71e33646a
GreyWolnick <52846222+GreyWolnick@users.noreply.github.com>
2023-05-19 00:27:47 -0700

    Update driver.py

--- driver.py
+++ driver.py
@@ -10,13 +10,73 @@ ks = [0.1, 0.5, 0.9]
 # cmd = f'python3 gce.py --reg_type max --dataset cifar10 --loss_func ce'
 # subprocess.run(cmd.split())
 
-for q in qs:
-    cmd = f'python3 main.py --dataset cifar10 --q {q} --k 0.4 --lam 0.0001'
+for i in range(3):  # VGCE fashion 0.5
+    cmd = f'python3 main.py --dataset fashionmnist --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.5 --sess {i}'
     subprocess.run(cmd.split())
 
-for k in ks:
-    cmd = f'python3 main.py --dataset cifar10 --q 0.5 --k {k} --lam 0.0001'
+for i in range(5):  # VGCE mnist 0.2
+    cmd = f'python3 main.py --dataset mnist --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.2 --sess {i}'
     subprocess.run(cmd.split())
+
+for i in range(5):  # VGCE fashion 0.2
+    cmd = f'python3 main.py --dataset fashionmnist --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.2 --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # VolMinNet fashion 0.2
+    cmd = f'python3 main.py --dataset fashionmnist --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.2 --loss_func ce --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # GCE mnist 0.2
+    cmd = f'python3 main.py --dataset mnist --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.2 --vol_min False --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # GCE mnist 0.5
+    cmd = f'python3 main.py --dataset mnist --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.5 --vol_min False --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # GCE fashion 0.2
+    cmd = f'python3 main.py --dataset fashionmnist --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.2 --vol_min False --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # GCE fashion 0.5
+    cmd = f'python3 main.py --dataset fashionmnist --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.5 --vol_min False --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # GCE cifar10 0.2
+    cmd = f'python3 main.py --dataset cifar10 --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.2 --vol_min False --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # GCE cifar10 0.5
+    cmd = f'python3 main.py --dataset cifar10 --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.5 --vol_min False --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # CE mnist 0.2
+    cmd = f'python3 main.py --dataset mnist --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.2 --vol_min False --loss_func ce --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # CE mnist 0.5
+    cmd = f'python3 main.py --dataset mnist --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.5 --vol_min False --loss_func ce --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # CE fashion 0.2
+    cmd = f'python3 main.py --dataset fashionmnist --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.2 --vol_min False --loss_func ce --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # CE fashion 0.5
+    cmd = f'python3 main.py --dataset fashionmnist --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.5 --vol_min False --loss_func ce --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # CE cifar10 0.2
+    cmd = f'python3 main.py --dataset cifar10 --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.2 --vol_min False --loss_func ce --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # CE cifar10 0.5
+    cmd = f'python3 main.py --dataset cifar10 --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.5 --vol_min False --loss_func ce --sess {i}'
+    subprocess.run(cmd.split())
+
+# for k in ks:
+#     cmd = f'python3 main.py --dataset cifar10 --q 0.5 --k {k} --lam 0.0001'
+#     subprocess.run(cmd.split())
 #
 # for lam in lams:
 #     cmd = f'python3 gce.py --reg_type max --dataset cifar10 --q 0.5 --k 0.4 --lam {lam}'
