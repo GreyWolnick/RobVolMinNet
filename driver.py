5298b16df15f41e15e850b15e23a9a10fd7425dc
GreyWolnick <52846222+GreyWolnick@users.noreply.github.com>
2023-06-02 10:28:36 -0700

    Update driver.py

--- driver.py
+++ driver.py
@@ -70,9 +70,31 @@ ks = [0.1, 0.5, 0.9]
 #     cmd = f'python3 main.py --dataset cifar10 --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.2 --vol_min False --loss_func ce --sess {i}'
 #     subprocess.run(cmd.split())
 
-for i in range(5):  # CE cifar10 0.5
-    cmd = f'python3 main.py --dataset cifar10 --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.5 --vol_min False --loss_func ce --sess {i}'
+for i in range(5):  # CE mnist 0.5
+    cmd = f'python3 main.py --dataset mnist --q 0.2 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.5 --loss_func ce --sess {i}'
     subprocess.run(cmd.split())
+
+for i in range(5):  # CE fashion 0.5
+    cmd = f'python3 main.py --dataset fashionmnist --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.5 --loss_func ce --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # CE cifar10 0.5
+    cmd = f'python3 main.py --dataset cifar10 --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.5 --loss_func ce --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # VGCE mnist 0.5
+    cmd = f'python3 main.py --dataset mnist --q 0.2 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.5 --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # VGCE fashion 0.5
+    cmd = f'python3 main.py --dataset fashionmnist --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.5 --sess {i}'
+    subprocess.run(cmd.split())
+
+for i in range(5):  # VGCE cifar10 0.5
+    cmd = f'python3 main.py --dataset cifar10 --q 0.5 --k 0.4 --lam 0.0001 --reg_type min --noise_rate 0.5 --sess {i}'
+    subprocess.run(cmd.split())
+
+
 
 # for k in ks:
 #     cmd = f'python3 main.py --dataset cifar10 --q 0.5 --k {k} --lam 0.0001'
