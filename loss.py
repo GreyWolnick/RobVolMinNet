37b11714d48c50cb58eadd19bf3fe77cfc8fdde8
GreyWolnick <52846222+GreyWolnick@users.noreply.github.com>
2023-06-12 18:43:29 -0700

    Update loss.py

--- loss.py
+++ loss.py
@@ -10,17 +10,17 @@ def symmetric_cross_entropy(alpha, beta):
 
         print("________________________")
         y_true_1 = y_true
-        y_pred_1 = y_pred
+        y_pred_temp = y_pred
 
-        y_true_2 = y_true
+        y_true_temp = y_true
         y_pred_2 = y_pred
 
         # y_pred_1 = torch.clamp(y_pred_1, 1e-7, 1.0)
         # y_true_2 = torch.clamp(y_true_2, 1e-4, 1.0)
         # y_pred_1 = torch.clamp(y_pred_1, min=1e-7, max=1.0)
         # y_true_2 = torch.clamp(y_true_2, min=1e-4, max=1.0)
-        y_pred_1 = torch.clamp(y_pred_1, min=0.5, max=1.0)
-        y_true_2 = torch.clamp(y_true_2, min=0.6, max=1.0)
+        y_pred_1 = torch.clamp(y_pred_temp, min=0.5, max=1.0)
+        y_true_2 = torch.clamp(y_true_temp, min=0.6, max=1.0)
 
         print("y_true_2")
         print(y_true_2)
