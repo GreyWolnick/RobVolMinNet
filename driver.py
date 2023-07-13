9e6364d71aafae658e35f64148eb949b63b8cc37
GreyWolnick <52846222+GreyWolnick@users.noreply.github.com>
2023-07-13 08:16:33 -0700

    Update driver.py

--- driver.py
+++ driver.py
@@ -4,27 +4,27 @@ percent_instance = [0.1, 0.2]
 
 for p in percent_instance:
     for i in range(5):  # Proposed
-        cmd = f'python3 main.py --dataset cifar10 --noise_rate 0.5 --lam 0.0001 --reg_type min --loss_func sl --alpha 0.9 --beta 0.4 --percent_instance_noise {p} --sess {i}'
+        cmd = f'python3 main.py --dataset fashionmnist --noise_rate 0.5 --lam 0.0001 --reg_type min --loss_func sl --alpha 0.9 --beta 0.4 --percent_instance_noise {p} --sess {i}'
         subprocess.run(cmd.split())
 
     for i in range(5):  # Proposed Max
-        cmd = f'python3 main.py --dataset cifar10 --noise_rate 0.5 --lam 0.0001 --reg_type max --loss_func sl --alpha 0.9 --beta 0.4 --percent_instance_noise {p} --sess {i}'
+        cmd = f'python3 main.py --dataset fashionmnist --noise_rate 0.5 --lam 0.0001 --reg_type max --loss_func sl --alpha 0.9 --beta 0.4 --percent_instance_noise {p} --sess {i}'
         subprocess.run(cmd.split())
 
     for i in range(5):  # Proposed no Regularization
-        cmd = f'python3 main.py --dataset cifar10 --noise_rate 0.5 --vol_min False --lam 0.0001 --reg_type min --loss_func sl --alpha 0.9 --beta 0.4 --percent_instance_noise {p} --sess {i}'
+        cmd = f'python3 main.py --dataset fashionmnist --noise_rate 0.5 --vol_min False --lam 0.0001 --reg_type min --loss_func sl --alpha 0.9 --beta 0.4 --percent_instance_noise {p} --sess {i}'
         subprocess.run(cmd.split())
 
     for i in range(5):  # VolMinNet
-        cmd = f'python3 main.py --dataset cifar10 --noise_rate 0.5 --lam 0.0001 --reg_type min --loss_func ce --percent_instance_noise {p} --sess {i}'
+        cmd = f'python3 main.py --dataset fashionmnist --noise_rate 0.5 --lam 0.0001 --reg_type min --loss_func ce --percent_instance_noise {p} --sess {i}'
         subprocess.run(cmd.split())
 
     for i in range(5):  # CE
-        cmd = f'python3 main.py --dataset cifar10 --noise_rate 0.5 --vol_min False --lam 0.0001 --reg_type min --loss_func ce --percent_instance_noise {p} --sess {i}'
+        cmd = f'python3 main.py --dataset fashionmnist --noise_rate 0.5 --vol_min False --lam 0.0001 --reg_type min --loss_func ce --percent_instance_noise {p} --sess {i}'
         subprocess.run(cmd.split())
 
     for i in range(5):  # GCE
-        cmd = f'python3 main.py --dataset cifar10 --noise_rate 0.5 --vol_min False --lam 0.0001 --reg_type min --loss_func gce --q 0.5 --k 0.4 --percent_instance_noise {p} --sess {i}'
+        cmd = f'python3 main.py --dataset fashionmnist --noise_rate 0.5 --vol_min False --lam 0.0001 --reg_type min --loss_func gce --q 0.5 --k 0.4 --percent_instance_noise {p} --sess {i}'
         subprocess.run(cmd.split())
 
 
