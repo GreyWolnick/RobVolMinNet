e94a7573e918978496246c593e63098ea0969619
GreyWolnick <52846222+GreyWolnick@users.noreply.github.com>
2023-07-13 15:13:45 -0700

    Update collect.py

--- collect.py
+++ collect.py
@@ -1,30 +1,79 @@
-import matplotlib.pyplot as plt
 import numpy as np
 
-averages = {}
-stds = {}
+def calculate_accuracy(dataset, loss_func, vol_min, reg_type, lam, instance_noise_rate):
+    alpha = "0.900000"
+    beta = "0.400000"
+    q = "0.700000"
+    k = "0.500000"
+    noise_rate = "0.500000"
 
-dataset = "cifar10"
-loss_func = "sl"
-vol_min = "True"
-reg_type = "max"
-alpha = "0.900000"
-beta = "0.400000"
-noise_rate = "0.500000"
-instance_noise_rate = "0.100000"
+    best_accs = []
+    for i in range(5):
+        if loss_func == "sl":
+            file_path = f"saves/{dataset}/{loss_func}/vol_min={vol_min}/{reg_type}/alpha={alpha}/beta={beta}/noise_rate_{noise_rate}/instance_noise_rate_{instance_noise_rate}/lam={lam}006_1/{i}/log.txt"
+        elif loss_func == "gce":
+            file_path = f"saves/{dataset}/{loss_func}/vol_min={vol_min}/{reg_type}/q={q}/k={k}/noise_rate_{noise_rate}/instance_noise_rate_{instance_noise_rate}/lam={lam}006_1/{i}/log.txt"
+        else:
+            file_path = f"saves/{dataset}/{loss_func}/vol_min={vol_min}/{reg_type}/noise_rate_{noise_rate}/instance_noise_rate_{instance_noise_rate}/lam={lam}006_1/{i}/log.txt"
 
-best_accs = []
-for i in range(5):
+        with open(file_path, 'r') as file:
+            for line in file:
+                if line.startswith("Best Model Test Loss"):
+                    desired_line = line.strip()
+                    best_accs.append(float(desired_line[-8:]))
+                    break
 
-    file_path = f"saves/{dataset}/{loss_func}/vol_min={vol_min}/{reg_type}/alpha={alpha}/beta={beta}/noise_rate_{noise_rate}/instance_noise_rate_{instance_noise_rate}/lam=0.0001006_1/{i}/log.txt"
-
-    with open(file_path, 'r') as file:
-        for line in file:
-            if line.startswith("Best Model Test Loss"):
-                desired_line = line.strip()
-                best_accs.append(float(desired_line[-8:]))
-                break
-
-print(f"{np.mean(best_accs)}±{np.std(best_accs)}")
+    print(dataset, loss_func, vol_min, reg_type, instance_noise_rate)
+    print(f"{np.mean(best_accs) * 100:.2f}±{np.std(best_accs) * 100:.3f}")
 
 
+configurations = [
+    {
+        'dataset': "cifar10",
+        'loss_func': "sl",
+        'vol_min': "True",
+        'reg_type': "min",
+        'lam': "0.0001"
+    },
+    {
+        'dataset': "cifar10",
+        'loss_func': "sl",
+        'vol_min': "True",
+        'reg_type': "max",
+        'lam': "0.0001"
+    },
+    {
+        'dataset': "cifar10",
+        'loss_func': "sl",
+        'vol_min': "False",
+        'reg_type': "min",
+        'lam': "0.0000"
+    },
+{
+        'dataset': "cifar10",
+        'loss_func': "ce",
+        'vol_min': "True",
+        'reg_type': "min",
+        'lam': "0.0001"
+    },
+    {
+        'dataset': "cifar10",
+        'loss_func': "ce",
+        'vol_min': "False",
+        'reg_type': "min",
+        'lam': "0.0000"
+    },
+    {
+        'dataset': "cifar10",
+        'loss_func': "gce",
+        'vol_min': "False",
+        'reg_type': "min",
+        'lam': "0.0000"
+    },
+]
+
+percent_instance = ["0.100000", "0.200000"]
+
+for config in configurations:
+    for p in percent_instance:
+        calculate_accuracy(config['dataset'], config['loss_func'], config['vol_min'], config['reg_type'], config['reg_type'], p)
