8bd3c454a98bbc2fc2c2d54e72ec4e1c55562492
GreyWolnick <52846222+GreyWolnick@users.noreply.github.com>
2023-07-24 12:29:49 -0700

    Update driver.py

--- driver.py
+++ driver.py
@@ -3,16 +3,18 @@ import subprocess
 percent_instance = [0.1, 0.2]
 sets = [(0.7, 0.1), (0.5, 0.4)]
 
-for set in sets:  # Proposed
-    cmd = f'python3 main.py --dataset cifar10 --noise_rate 0.3 --lam 0.0001 --reg_type min --loss_func sl --alpha {set[0]} --beta {set[1]} --percent_instance_noise 0.1 --sess 0'
-    subprocess.run(cmd.split())
+# best so far (0.7, 0.4)
+
+# for set in sets:  # Proposed
+#     cmd = f'python3 main.py --dataset cifar10 --noise_rate 0.3 --lam 0.0001 --reg_type min --loss_func sl --alpha {set[0]} --beta {set[1]} --percent_instance_noise 0.1 --sess 0'
+#     subprocess.run(cmd.split())
 
 
-# for p in percent_instance:
-#     for i in range(5):  # Proposed
-#         cmd = f'python3 main.py --dataset cifar10 --noise_rate 0.3 --lam 0.0001 --reg_type min --loss_func sl --alpha 0.9 --beta 0.4 --percent_instance_noise {p} --sess {i}'
-#         subprocess.run(cmd.split())
-#         print(f"Finished iteration {i+1} of proposed")
+for p in percent_instance:
+    for i in range(5):  # Proposed
+        cmd = f'python3 main.py --dataset cifar10 --noise_rate 0.3 --lam 0.0001 --reg_type min --loss_func sl --alpha 0.5 --beta 0.4 --percent_instance_noise {p} --sess {i}'
+        subprocess.run(cmd.split())
+        print(f"Finished iteration {i+1} of proposed")
 #
 #     for i in range(5):  # Proposed Max
 #         cmd = f'python3 main.py --dataset cifar10 --noise_rate 0.3 --lam 0.0001 --reg_type max --loss_func sl --alpha 0.9 --beta 0.4 --percent_instance_noise {p} --sess {i}'
