d64322747edce4ca6f90d784571fe86ad21bb872
GreyWolnick <52846222+GreyWolnick@users.noreply.github.com>
2023-07-24 18:29:14 -0700

    Update driver.py

--- driver.py
+++ driver.py
@@ -12,7 +12,7 @@ sets = [(0.7, 0.1), (0.5, 0.4)]
 
 for p in percent_instance:
     for i in range(5):  # Proposed
-        cmd = f'python3 main.py --dataset cifar10 --noise_rate 0.3 --lam 0.0001 --reg_type min --loss_func sl --alpha 0.5 --beta 0.4 --percent_instance_noise {p} --sess {i}'
+        cmd = f'python3 main.py --dataset cifar10 --noise_rate 0.3 --lam 0.0001 --reg_type min --loss_func sl --alpha 0.7 --beta 0.4 --percent_instance_noise {p} --sess {i}'
         subprocess.run(cmd.split())
         print(f"Finished iteration {i+1} of proposed")
 #
@@ -36,10 +36,10 @@ for p in percent_instance:
 #         subprocess.run(cmd.split())
 #         print(f"Finished iteration {i + 1} of ce")
 #
-#     for i in range(5):  # GCE
-#         cmd = f'python3 main.py --dataset cifar10 --noise_rate 0.3 --vol_min False --lam 0.0001 --reg_type min --loss_func gce --q 0.7 --k 0.5 --percent_instance_noise {p} --sess {i}'
-#         subprocess.run(cmd.split())
-#         print(f"Finished iteration {i + 1} of gce")
+    for i in range(5):  # GCE
+        cmd = f'python3 main.py --dataset cifar10 --noise_rate 0.3 --vol_min False --lam 0.0001 --reg_type min --loss_func gce --q 0.7 --k 0.5 --percent_instance_noise {p} --sess {i}'
+        subprocess.run(cmd.split())
+        print(f"Finished iteration {i + 1} of gce")
 
 
 
